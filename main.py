8e4462a78ac492ed264d6ee545e067d05db78265
Snehamn24 <snehamn216@gmail.com>
2025-10-26 21:00:02 +0530

    Imported necessary modules

---
+++ main.py
@@ -0,0 +1,12 @@
+#import the libraries
+
+from tkinter import * #imports all the modules from the tkinter label,button,textbox
+
+from tkinter import ttk #imports themed tkinter module which provides the modern widgets
+
+from PIL import Image , ImageTk #these are used to work with the images 
+
+#Image -> for open,manipulate and save
+#ImageTk -> Used to convert PIL images into Tkinter-compatible format (so they can be displayed in a GUI).
+
+
