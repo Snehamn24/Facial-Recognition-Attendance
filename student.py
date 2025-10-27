7960746d08d6696027405d933fcdbd7cda934f46
Snehamn24 <snehamn216@gmail.com>
2025-10-27 20:51:03 +0530

    Added image and left and right label frame

--- student.py
+++ student.py
@@ -8,6 +8,23 @@ class Student:
         self.root.geometry("1530x790+0+0")
         self.root.title("Student")
 
+         #upper image
+        img1 = Image.open(r"C:\repo\Facial-Recognition-Attendance\face_bg.webp")
+        img1 = img1.resize((1530,790))
+        self.img1 = ImageTk.PhotoImage(img1)
+
+        label1 = Label(self.root,image=self.img1)
+        label1.place(x=0,y=0,width=1530,height=200)
+
+        #left label frame
+        left_frame = LabelFrame(self.root,text="Student Details",bd = 2,relief=RIDGE)
+        left_frame.place(x=8,y=200,width=730,height=580)
+
+        #right label frame
+        right_frame = LabelFrame(self.root,text="Student Details",bd=2,relief=RIDGE)
+        right_frame.place(x=750,y=200,width=730,height=580)
+
+
 
 if __name__=="__main__":
     root = Tk()
