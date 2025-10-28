9275ff65e5e3927a7934e95f06e9fa189e71ecbc
Snehamn24 <snehamn216@gmail.com>
2025-10-28 19:57:13 +0530

    Creating the login page

---
+++ login.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from tkinter import ttk,messagebox
+import cv2
+import csv
+import os
+import datetime
+
+class Login:
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry(400x300)
+        delf.root.title("Login Page")
+
+        self.var_regno = StringVar()
+        self.var_password = StringVar()
+
+        Label(self.root,text="student Login",font=("Times New Roman",16,"bold")).pack(pady=15)
+
+        frame = Frame(self.root, padx=10, pady=10, relief=RIDGE, bd=2)
+        frame.pack(pady=10)
+
+        Label(frame, text="Reg No:", font=("Times New Roman", 12)).grid(row=0, column=0, sticky=W, pady=5)
+        Entry(frame, textvariable=self.var_regno, width=25).grid(row=0, column=1, pady=5)
+
+        Label(frame, text="Password:", font=("Times New Roman", 12)).grid(row=1, column=0, sticky=W, pady=5)
+        Entry(frame, textvariable=self.var_password, show="*", width=25).grid(row=1, column=1, pady=5)
+
+        Button(frame, text="Login", command=self.login_student, width=15, bg="blue", fg="white").grid(row=2, columnspan=2, pady=10)
+
+        
+
